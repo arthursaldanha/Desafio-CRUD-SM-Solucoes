da38aac37611bf38458572546b9880c7fe0167d8
Arthur Saldanha <saldanhadev@gmail.com>
2021-11-27 19:03:46 -0300

    fix: corrigindo o loader

--- src/components/Loading/Styled.js
+++ src/components/Loading/Styled.js
@@ -1,34 +1,31 @@
 import styled, { keyframes } from "styled-components";
 
 const IsRotating = keyframes`
-	0% {
-  	transform: rotate(0)
-  }
-	100% {
-  	transform: rotate(360deg)
+	to {
+  	transform: rotate(1turn)
   }
 `;
 
 export const Container = styled.div`
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.4);
-  display: flex;
-  height: 100%;
-  justify-content: center;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  z-index: 1;
+	align-items: center;
+	background-color: rgba(0, 0, 0, 0.4);
+	display: flex;
+	height: 100%;
+	justify-content: center;
+	left: 0;
+	position: absolute;
+	top: 0;
+	width: 100%;
+	z-index: 1;
 `;
 
 export const Loader = styled.div`
-  animation-name: ${IsRotating};
-  animation-duration: 2s;
-  animation-iteration-count: infinite;
-  border-radius: 50%;
-  border-top-color: #184e77;
-  border: 6px solid #fff;
-  height: 80px;
-  width: 80px;
+	animation-name: ${IsRotating};
+	animation-duration: 1s;
+	animation-iteration-count: infinite;
+	border: 6px solid #e5e5e5;
+	border-radius: 50%;
+	border-top-color: #184e77;
+	height: 80px;
+	width: 80px;
 `;
